780ca878cd9eb0d4d9e0e2b734428edc57e848bd
Miguel Angel Viera Gonzalez <mangelviera93@gmail.com>
2015-08-07 22:44:05 +0100

    green: when create a document'  it 'saves it on the blog'

--- src/business/post_service.rb
+++ src/business/post_service.rb
@@ -1,5 +1,11 @@
+require_relative 'post'
+
 class PostService
+  def initialize(repository)
+    @repository = repository
+  end
+
   def create (post)
-    ;
+    @repository.save(post)
   end
 end
---
+++ test/blog_post_service_spec.rb
@@ -0,0 +1,17 @@
+require 'rspec'
+require_relative '../src/business/post_service'
+require_relative '../src/business/post'
+
+describe 'Blog Post Service' do
+  context 'when create a document' do
+    it 'saves it on the blog' do
+      repository = spy 'BlogPostsRepository'
+      service = PostService.new repository
+
+      post = Post.new('any title', 'any content')
+      service.create post
+
+      expect(repository).to have_received(:save)
+    end
+  end
+end
